2b0cb8dfdcb3fe5457b9de50f26d2fa81c1399c4
rubenjr0 <rubenjr0@proton.me>
2026-01-05 15:10:52 +0100

    pest mini change and test

--- crates/parser/src/lib.rs
+++ crates/parser/src/lib.rs
@@ -1,4 +1,5 @@
-use winnow::{Parser, ascii::multispace0, combinator::delimited, error::ParserError};
+use pest_derive::Parser;
+use winnow::{Parser as Pw, ascii::multispace0, combinator::delimited, error::ParserError};
 
 mod custom_types;
 pub mod expressions;
@@ -9,9 +10,27 @@ mod statements;
 mod top_level;
 mod types;
 
-pub fn ws<'a, F, O, E: ParserError<&'a str>>(inner: F) -> impl Parser<&'a str, O, E>
+pub fn ws<'a, F, O, E: ParserError<&'a str>>(inner: F) -> impl Pw<&'a str, O, E>
 where
-    F: Parser<&'a str, O, E>,
+    F: Pw<&'a str, O, E>,
 {
     delimited(multispace0, inner, multispace0)
+}
+
+#[derive(Parser)]
+#[grammar = "../../../grammar.pest"]
+struct Grammar;
+
+#[cfg(test)]
+mod tests {
+    use pest::Parser;
+
+    use super::*;
+
+    #[test]
+    fn sample_test() {
+        let input = include_str!("../../../samples/sample.elk");
+        let result = Grammar::parse(Rule::Program, input);
+        result.expect("Failed to parse input");
+    }
 }
